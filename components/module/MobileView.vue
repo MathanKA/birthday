a527b8a7d2e64a4b4f0b65cd6bc7bfbdab83db18
Mathan K A <mathan@cyware.com>
2020-05-05 12:06:45 +0530

    feat(mobile and desktop): views are seperated

---
+++ components/module/MobileView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <section id="b-intro" class="h-100 pb-0">
+      <b-container class="h-100">
+        <b-row class="h-100">
+          <b-col class="text-center align-self-center">
+            <h1>
+              Hey Baby, <br />
+              Seems you are viewing in small devices <br />
+              Sorry, this small space is not enough to celebrate your
+              "Birthday"<br />
+              <strong>Please open it in Laptop</strong>
+            </h1>
+          </b-col>
+        </b-row>
+      </b-container>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,17 @@
 <template>
   <div>
-    <section id="b-intro">
-      <client-only>
-        <wind-banners></wind-banners>
-      </client-only>
-      <b-container fluid>
-        <b-row>
-          <b-col cols="2">
-            <balloon-group></balloon-group>
-          </b-col>
-          <b-col cols="8" class="text-center">
-            <wish></wish>
-            <cake @releaseBalloons="triggerBalloons"></cake>
-          </b-col>
-          <b-col cols="2">
-            <balloon-group></balloon-group>
-          </b-col>
-        </b-row>
-        <balloons
-          class="flying-balloons"
-          :release-balloons="releaseBalloons"
-        ></balloons>
-      </b-container>
-    </section>
-    <section id="b-info">
-      <b-container>
-        <b-row>
-          <b-col cols="8">
-            <photo-booth></photo-booth>
-          </b-col>
-          <b-col cols="4" class="align-self-center">
-            <p class="text-left">
-              <span style="font-size: 2rem;">&ldquo;</span>Sometimes, we may
-              feel who we are, who we want to be, where exactly we supposed to
-              be. but if you notice closely, everything will make sense.<span
-                style="font-size: 2rem;"
-                >&rdquo;</span
-              >
-            </p>
-          </b-col>
-        </b-row>
-      </b-container>
-    </section>
+    <desktop-view class="desktop"> </desktop-view>
+    <mobile-view class="mobile"></mobile-view>
   </div>
 </template>
 
 <script>
-import BalloonGroup from '@/components/BalloonGroup'
-import Cake from '@/components/Cake'
-import PhotoBooth from '@/components/PhotoBooth'
-import Wish from '@/components/Wish'
-// import FireConfetti from '@/components/FireConfetti'
+import DesktopView from '@/components/module/DesktopView'
+import MobileView from '@/components/module/MobileView'
 export default {
   components: {
-    'wind-banners': () => import('@/components/WindBanners.vue'),
-    balloons: () => import('@/components/Balloons.vue'),
-    BalloonGroup,
-    Cake,
-    PhotoBooth,
-    Wish
-  },
-  data() {
-    return { releaseBalloons: false, balloonKey: 0 }
-  },
-  methods: {
-    triggerBalloons() {
-      this.releaseBalloons = !this.releaseBalloons
-    }
+    DesktopView,
+    MobileView
   }
 }
 </script>
